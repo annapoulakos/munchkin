d5869a8a9b3eba28c97e1afe6a9b88c1b3c4aa84
Tom Neyland <tcneyland+github@gmail.com>
2015-05-24 15:34:15 -0400

    fix(gulp): Removes old task from watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('browserify', function () {
 // pretty sure this watch regex will work for you but don't trust it
 // if you feel that it is not
 gulp.task('watch', function () {
-    gulp.watch('./**/*.[html|js|sass|scss|json|es6]', ['browserify', 'b2']);
+    gulp.watch('./**/*.[html|js|sass|scss|json|es6]', ['browserify']);
 });
 
 gulp.task('default', ['watch']);
